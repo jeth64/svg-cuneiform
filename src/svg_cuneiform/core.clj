64056a3921be5c3eea8dd98d9ee557733c94118b
jeth64 <jeth64@arcor.de>
2014-02-17 04:22:18 +0100

    Add reference points

--- src/svg_cuneiform/core.clj
+++ src/svg_cuneiform/core.clj
@@ -11,15 +11,15 @@
 (def translations (get-translations file layer-id))
 (def paths (get-paths file layer-id translations))
 (def reduced-paths (reduce #(update-in %1 [%2] reduce-path) paths (keys paths)))
+(def references (reduce #(update-in %1 [%2] intersection) paths (keys paths)))
 (def lines (get-lines file layer-id translations))
 (def outfile "test/svg_cuneiform/images/out.svg")
 (def delete ["path8055"])
 (def new-paths []) ;;[[(second (k-means curve 4))]]
 
-
-(let [paths (get-paths file layer-id translations)]
-  (reduce #(update-in %1 [%2] reduce-path) paths (keys paths))
-  )
+;;
+;; helper functions
+;;
 
 (defn- euclidean-squared [p1 p2]
   (reduce #(+ %1 (* %2 %2)) 0 (map - p1 p2)))
@@ -27,6 +27,20 @@
 (defn- average [ptlist]
   (if (< (count ptlist) 1) nil
       (map #(/ % (count ptlist)) (apply map + ptlist))))
+
+(defn- intersection
+  "Returns intersection of lines through p1 and p2 and p3 and p4 respectively.
+   Returns average of points if lines are parallel"
+  [ptlist]
+  (let [[p1 p2 p3 p4] ptlist
+        [a1r a2] [(reverse (map - p1 p2)) (map - p3 p4)]
+        b (map - p3 p1)
+        determinant (apply - (map * a1r a2))
+        l (print ptlist)]
+    (if (> (Math/abs determinant) 0.000001)
+      (map #(- %1 (* %2 (/ (apply - (map * a1r b)) determinant))) p3 a2)
+      (average ptlist))))
+
 
 
 (defn k-means
@@ -59,28 +73,31 @@
             [new-labels centroids])))))
 
 
-(defn reduce-path [ptlist]
+(defn k-means-reduce [ptlist]
   (letfn [(order-points [points]
             (apply min-key #(reduce + (map euclidean-squared % (rest %)))
                    (permutations points)))]
     (if (> (count ptlist) 4) (order-points (second (k-means ptlist 4))) ptlist)))
 
+(defn regression-reduce [ptlist]
+  ptlist)
 
-(second (first reduced-paths))
 
 (defn main []
   (let [layer-id "cuneiforms"
-        file (get-svg "test/svg_cuneiform/images/3-1.svg")
+        file (get-svg "test/svg_cuneiform/images/1-0.svg")
         outfile "test/svg_cuneiform/images/out.svg"
         translations (get-translations file layer-id)
         paths (get-paths file layer-id translations)
         lines (get-lines file layer-id translations)
-        reduced (reduce #(update-in %1 [%2] reduce-path) paths (keys paths))
+        reduced-paths (reduce #(update-in %1 [%2] k-means-reduce) paths (keys paths))
+        references (reduce #(update-in %1 [%2] intersection) reduced-paths (keys paths))
 
-        new-paths []
-        to-delete [(keys paths)]
+        ;new-paths (apply merge (map #(vector (second %)) reduced-paths) (map #(vector (repeat 4 (second %))) references))
+        new-paths (map #(vector (cons [0 0] (repeat 3 (second %)))) references)
+        to-delete [];[(keys paths)]
         ]
-    (update-and-save file layer-id (map #(vector (second %)) reduced) to-delete outfile)
+    (update-and-save file layer-id new-paths to-delete outfile)
 
     ))
 
