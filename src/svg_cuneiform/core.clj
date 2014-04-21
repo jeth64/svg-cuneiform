db7bf4f50e121bbff31f2be0c72aae6557529883
jeth64 <j.massa@mail.de>
2014-04-21 20:02:37 +0200

    introduce default configuration

--- src/svg_cuneiform/core.clj
+++ src/svg_cuneiform/core.clj
@@ -6,83 +6,52 @@
              [get-svg get-translations get-paths get-lines
               update-and-save update-file]]))
 
-
-
-(def test-config2
-  {:filename "test/svg_cuneiform/images/2-1.svg"
-   :layer-id "cuneiforms"
-   :outfile "test/svg_cuneiform/images/out.svg"
-   ;; parameters
-   :curve-line-threshold 0.0
-   :extension-dist 0.7
-   :cos-allowed-angle 0.7
-   ;; style attributes
-   :stroke-width 0.5
-   :color "blue"
-   })
-
-(def test-config3
-  {:filename "test/svg_cuneiform/images/3-1.svg"
-   :layer-id "cuneiforms"
-   :outfile "test/svg_cuneiform/images/out.svg"
-   ;; parameters
-   :curve-line-threshold 1.0
-   :extension-dist 1.0
-   :cos-allowed-angle 0.6
-   ;; style attributes
-   :stroke-width 0.5
-   :color "blue"
-   })
-
-(def config1
-  {:filename "test/svg_cuneiform/images/VAT_10833-SeiteB_HPSchaudig.svg"
-   :layer-id "g20"
-   :outfile "test/svg_cuneiform/images/out.svg"
-   :curve-line-threshold 0.0
-   :extension-dist 0.7
-   :cos-allowed-angle 0.7
-   :stroke-width 0.5
-   :color "blue"
-   })
-(def config2
-  {:filename "test/svg_cuneiform/images/VAT_09671_Rs_SJakob.svg"
-   :layer-id "Autographie"
-   :outfile "test/svg_cuneiform/images/out.svg"
-   :curve-line-threshold 0.0
-   :extension-dist 0.7
-   :cos-allowed-angle 0.7
-   :stroke-width 0.5
-   :color "blue"
-   })
-(def config3
-  {:filename "test/svg_cuneiform/images/VAT_10908_Vs.svg"
-   :layer-id "Kopie"
-   :outfile "test/svg_cuneiform/images/out.svg"
-   :curve-line-threshold 0.0
-   :extension-dist 0.7
-   :cos-allowed-angle 0.7
-   :stroke-width 0.5
-   :color "blue"
-   })
-
-(def test-config1
+(def default-config
   {:filename "test/svg_cuneiform/images/1-1.svg"
    :layer-id "cuneiforms"
    :outfile "test/svg_cuneiform/images/out.svg"
-   ;; parameters
+   ;; parameter
    ;; - for classification
-   :curve-line-threshold 0.0
+   :curve-line-threshold 0.0 ;; no curves as lines
    ;; - for finding extensions
-   :extension-dist 0.7
-   :cos-allowed-angle 0.3
+   :extension-dist 1.0
+   :cos-allowed-angle 0.5
    ;; style attributes
    :stroke-width 0.5
-   :color "blue"
-   })
+   :color "blue"})
+
+(def test-config1
+  {:filename "test/svg_cuneiform/images/1-1.svg"})
+
+(def test-config2
+  {:filename "test/svg_cuneiform/images/2-1.svg"
+   :curve-line-threshold 0.0
+   :extension-dist 0.8
+   :cos-allowed-angle 0.7})
+
+(def test-config3
+  {:filename "test/svg_cuneiform/images/3-1.svg"
+   ;; parameters
+   :curve-line-threshold 1.0
+   :extension-dist 1.0
+   :cos-allowed-angle 0.6})
+
+(def config1
+  {:filename "test/svg_cuneiform/images/VAT_10833-SeiteB_HPSchaudig.svg"
+   :layer-id "g20"})
+
+(def config2
+  {:filename "test/svg_cuneiform/images/VAT_09671_Rs_SJakob.svg"
+   :layer-id "Autographie"})
+
+(def config3
+  {:filename "test/svg_cuneiform/images/VAT_10908_Vs.svg"
+   :layer-id "Kopie"})
+
 
 ;;(time (main))
 (time ;; defn main []
- (let [config test-config2
+ (let [config (merge default-config test-config3)
        style {:fill "none" :stroke (config :color) :stroke-width (config :stroke-width)}
        file (get-svg (config :filename))
        translations (get-translations file (config :layer-id))
@@ -92,7 +61,7 @@
 
        a (print [(count curve-map) (count line-map)])
 
-       [wedges used-keys] (find-wedges curve-map)
+       [wedges used-keys] (find-wedges curve-map (config :max-merge-dist))
        [wedges2 path-keys] (add-extension wedges lines (config :extension-dist) (config :cos-allowed-angle))
        ;;c (print (add-extension wedges line-map))
 
