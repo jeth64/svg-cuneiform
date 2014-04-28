1ab541df45e4effae60a413992db6d98cc42b2c0
jeth64 <j.massa@mail.de>
2014-04-28 22:59:14 +0200

    add tests for 'round' and 'cubic-zeros'

--- test/svg_cuneiform/math_test.clj
+++ test/svg_cuneiform/math_test.clj
@@ -1,21 +1,28 @@
 (ns svg-cuneiform.math-test
   (:require [clojure.test :refer :all]
+            [midje.sweet :refer [facts fact]]
             [svg-cuneiform.math :refer :all]))
 
 
-(deftest test-cubic-zeros
-  (testing "D>0"
-    (let [roots (cubic-zeros 1 0 6 -20)]
-      (is (= 1 (count roots)))
-      (is (= 2 (first (round 0 (first roots)))))))
-  (testing "D<0"
-    (let [roots (cubic-zeros 1 -3 -144 432)]
-      (is (= 3 (count roots)))
-      (is (= '(-12 3 12) (sort (map (partial round 0) roots)))))))
+(facts "About 'round'"
+       (fact "integer rounding succeeds"
+             (round 0 2.5) => 3)
+       (fact "decimal rounding succeeds"
+             (round 1 2.05) => 2.1))
 
-
-(map (partial polynomial [1 -3 -144 432])
-     (cubic-zeros 1 -3 -144 432)) ;; sollte [0 0 0] sein
-
-
-(bezier-coeffs [1 2 3 4])
+(facts "About 'cubic-zeros'"
+       (fact "works for an example with D>0"
+             (map int (cubic-zeros 1 0 6 -20))
+             => [2])
+       (fact "works for an example with D<0"
+             (sort (map round (cubic-zeros 1 -3 -144 432)))
+             => [-12 3 12])
+       (fact "works for random integer coefficients"
+             (take 5 (repeatedly
+                      #(let [coeffs (take 4 (repeatedly (fn [] (rand-int 1000))))
+                            res (map (comp round (partial polynomial coeffs))
+                                     (apply cubic-zeros coeffs))]
+                        (if (every? zero? res)
+                          true
+                          (str "not true for " (vec coeffs))))))
+             => (partial every? true?)))
