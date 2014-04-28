2ba61851ea81a0987f929b4bab09806c91868d6c
jeth64 <j.massa@mail.de>
2014-04-28 16:53:22 +0200

    use midje for testing

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.pallix/analemma "1.0.0"]
-                 [incanter "1.5.4"]])
+                 [incanter "1.5.4"]
+                 [midje "1.6.3"]])
