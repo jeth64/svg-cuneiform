4299f94dd77ff32fc6cb9aa21570badf1f3d938f
jeth64 <j.massa@mail.de>
2014-04-29 14:14:13 +0200

    switch to midje.sweet

--- test/svg_cuneiform/svg_test.clj
+++ test/svg_cuneiform/svg_test.clj
@@ -1,50 +1,37 @@
 (ns svg-cuneiform.svg-test
   (:require [clojure.test :refer :all]
+            [midje.sweet :refer [facts fact]]
             [svg-cuneiform.svg :refer [parse-transform parse-path matrix2path]]))
 
 
-(def res-single '(["translate" (543.654 544.65)]))
-(def res-multi '(["translate" (543.654 544.65)] ["scale" (643 535)]))
-(def single-transform "translate(543.654,544.65)")
-(def multi-transform "translate(543.654,544.65) scale(643, 535)")
-
-(def pt-list ['(100 200) '(100 100) '(250 100) '(250 200) '(250 300) '(400 300) '(400 200)])
-(def C-path "M100,200 C100,100 250,100 250,200 C250,300 400,300 400,200")
-(def c-path "M100,200 c0,-100 150,-100 150,0 c0,100 150,100 150,0")
-(def CS-path "M100,200 C100,100 250,100 250,200 S400,300 400,200")
-(def cs-path "M100,200 c0,-100 150,-100 150,0 s150,100 150,0")
-;; parse-path might not work with Cc or Cs
+(parse-path "m 250.342,267.458 c 0.342,1.532 1.57,2.74 1.186,4.396 -0.327,1.409 -1.111,2.8 -1.91,3.995 -0.18,0.269 0.253,0.519 0.432,0.252 0.887,-1.326 1.663,-2.831 2.025,-4.393 0.371,-1.598 -0.922,-2.913 -1.25,-4.383 -0.071,-0.315 -0.553,-0.181 -0.483,0.133 l 0,0 z")
 
 
-(deftest test-parse-single-transform
-  (let [transforms (parse-transform single-transform)]
-    (is (= (count transforms) 1))
-    (is (= (count (first transforms)) 2))
-    (is (= transforms res-single))))
+(facts "About 'parse-transform'"
+       (fact "can handle single transformation"
+             (parse-transform "translate(543.654,544.65)")
+             => '(["translate" (543.654 544.65)]))
+       (fact "can handle multiple transformations"
+             (parse-transform  "translate(543.654,544.65) scale(643, 535)")
+             => '(["translate" (543.654 544.65)] ["scale" (643 535)])))
 
-(deftest test-parse-multi-transform
-  (let [transforms (parse-transform multi-transform)]
-    (is (= (count transforms) 2))
-    (is (= (count (second transforms)) 2))
-    (is (= transforms res-multi))))
+(facts "About 'parse-path'"
+       (fact "can handle path with C"
+             (parse-path "M100,200 C100,100 250,100 250,200 C250,300 400,300 400,200")
+             => ['(100 200) '(100 100) '(250 100) '(250 200) '(250 300) '(400 300) '(400 200)])
+       (fact "can handle path with c"
+             (parse-path "M100,200 c0,-100 150,-100 150,0 c0,100 150,100 150,0")
+             => ['(100 200) '(100 100) '(250 100) '(250 200) '(250 300) '(400 300) '(400 200)])
+       (fact "can handle path with C and S"
+             (parse-path  "M100,200 C100,100 250,100 250,200 S400,300 400,200")
+             => ['(100 200) '(100 100) '(250 100) '(250 200) '(250 300) '(400 300) '(400 200)])
+       (fact "can handle path with c and s"
+             (parse-path "M100,200 c0,-100 150,-100 150,0 s150,100 150,0")
+             => ['(100 200) '(100 100) '(250 100) '(250 200) '(250 300) '(400 300) '(400 200)]))
 
-
-(deftest test-parse-path-with-C
-  (is (= (parse-path C-path) pt-list)))
-
-(deftest test-parse-path-with-c
-  (is (= (parse-path c-path) pt-list)))
-
-(deftest test-parse-path-with-S
-  (is (= (parse-path CS-path) pt-list)))
-
-(deftest test-parse-path-with-s
-  (is (= (parse-path cs-path) pt-list)))
-
-
-(deftest test-matrix2path-number-amount
-  (is (= (* 2 (count pt-list))
-         (count (re-seq  #"-?(?:\d*\.?)\d+" (matrix2path (partition 4 3 pt-list)))))))
-
-
-(run-tests 'svg-cuneiform.svg-test)
+(facts "About 'matrix2path'"
+       (fact "the result string has the right amount of numbers in it"
+             (count (re-seq  #"-?(?:\d*\.?)\d+"
+                             (matrix2path [['(100 200) '(100 100) '(250 100) '(250 200)]
+                                           ['(250 200) '(250 300) '(400 300) '(400 200)]])))
+             => (count (flatten ['(100 200) '(100 100) '(250 100) '(250 200) '(250 300) '(400 300) '(400 200)]))))
