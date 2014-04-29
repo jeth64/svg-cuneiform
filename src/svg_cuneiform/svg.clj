8c6ec81b4524d91abd339db952a8940b727e07e0
jeth64 <j.massa@mail.de>
2014-04-29 16:02:02 +0200

    Make parsing of M and L commands possible and implement transformation to beziers

--- src/svg_cuneiform/svg.clj
+++ src/svg_cuneiform/svg.clj
@@ -1,5 +1,6 @@
 (ns svg-cuneiform.svg
-  (:require [clojure.string :refer [join split trim]]))
+  (:require [clojure.string :refer [join split trim]]
+            [svg-cuneiform.math :refer [round]]))
 
 ;; Unchecked parsing: invalid transform or path strings will cause unknown behaviour
 
@@ -12,7 +13,7 @@
      (map #(vector (second %)
                    (partition 2 (map (partial read-string)
                                      (re-seq #"-?(?:\d*\.?)\d+" (last %)))))
-          (re-seq #"([mMcCsS])([^A-Za-z]+)" str)))
+          (re-seq #"([mMcCsSlL])([^A-Za-z]+)" str)))
 
 (defn- add-first-cntrl-pts [first offset points]
   [offset (cons first
@@ -36,6 +37,40 @@
 
 
 ;;
+;; Functionality for extracting points from path string
+;;
+
+(defn parse-path
+  "Returns list of absolute points (length: 1+3*x)."
+  [polybezier-string]
+  (let [cmd-numbers-list (get-char-ptlist-pairs polybezier-string)
+        first-pt (first (second (first cmd-numbers-list)))]
+    (loop [offset first-pt i 0 points [first-pt]]
+      (if (< i (count cmd-numbers-list))
+        (let [[cmd numbers] (nth cmd-numbers-list i)
+              [offset new-pts] (case cmd
+                                 "M" [(last numbers) (lines2beziers (rest numbers))]
+                                 "m" (make-absolute (first numbers) (lines2beziers (rest numbers)) 3)
+                                 "C" [offset numbers]
+                                 "c" (make-absolute offset numbers 3)
+                                 "S" (add-first-cntrl-pts
+                                      (map + (last points)
+                                           (apply map - (reverse (take-last 2 points))))
+                                      offset numbers)
+                                 "s" (apply add-first-cntrl-pts
+                                            (map + (last points)
+                                                 (apply map - (reverse (take-last 2 points))))
+                                            (make-absolute offset numbers 2))
+                                 "L" [(last numbers) (lines2beziers numbers)]
+                                 "l" (make-absolute offset (lines2beziers numbers) 3)
+                                 )]
+          (recur offset (inc i) (reduce conj points new-pts) ))
+        points))))
+
+;;(defn ptlist2path [ptlist] (matrix2path (partition 4 3 ptlist)))
+
+
+;;
 ;; Functionality for retrieving transformations
 ;;
 
@@ -49,34 +84,8 @@
          (re-seq #"(\w+)\(([^)]*)\)" (trim str)))
     '() ))
 
-
-;;
-;; Functionality for extracting points from path string
-;;
-
-(defn parse-path
-  "Returns list of absolute points (length: 1+3*x)."
-  [polybezier-string]
-  (let [cmd-numbers-list (get-char-ptlist-pairs polybezier-string)
-        first-pt (first (second (first cmd-numbers-list)))]
-    (loop [offset first-pt i 1 points [first-pt]]
-      (if (< i (count cmd-numbers-list))
-        (let [[cmd numbers] (nth cmd-numbers-list i)
-              [offset new-pts] (case cmd
-                                 "c" (make-absolute offset numbers 3)
-                                 "C" [offset numbers]
-                                 "s" (apply add-first-cntrl-pts
-                                            (map + (last points)
-                                                 (apply map - (reverse (take-last 2 points))))
-                                            (make-absolute offset numbers 2))
-                                 "S" (add-first-cntrl-pts
-                                      (map + (last points)
-                                           (apply map - (reverse (take-last 2 points))))
-                                      offset numbers))]
-          (recur offset (inc i) (reduce conj points new-pts) ))
-        points))))
-
-;(defn path2matrix [pathstring] (partition 4 3 (parse-path pathstring)))
+(defn lines2beziers [line-pts]
+  (apply concat (map (partial repeat 3) line-pts)))
 
 
 ;;
@@ -87,11 +96,11 @@
 (defn matrix2path
   "Return string for polybezier curve (input: nx4x2-matrix)"
   [matrix]
-  (letfn [(matrix-round [mat]
-            (map (partial map (partial map #(float (/ (Math/round (* (float %) 1000)) 1000))))
-                 mat))]
-    (let [mat (matrix-round matrix)
-          M (str "M" (join "," (first (first mat))))
-          C (join " " (map #(str "C" (join " " (map (partial join ",")
-                                                    (rest %)))) mat))]
-      (str M " " C))))
+  (let [mat (map (partial map (partial map (partial round 3))) matrix)
+        M (str "M" (join "," (first (first mat))))
+        C (join " " (map #(str "C" (join " " (map (partial join ",")
+                                                  (rest %)))) mat))]
+    (str M " " C)))
+
+
+;(defn path2matrix [pathstring] (partition 4 3 (parse-path pathstring)))
